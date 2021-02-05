8d51213650df0d83a2079eaa9320c8b6d6eb4d1f
Piotr Kolacz <pkolacz@gradle.com>
2021-02-05 22:59:37 +0100

    Implemented FileBasedDatabase

--- src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
+++ src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
@@ -1,21 +1,19 @@
 package pl.futurecollars.invoicing.model;
 
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InvoiceEntry {
 
   private String description;
+  private int quantity;
   private BigDecimal price;
   private BigDecimal vatValue;
   private Vat vatRate;
-
-  public InvoiceEntry(String description, BigDecimal price, BigDecimal vatValue, Vat vatRate) {
-    this.description = description;
-    this.price = price;
-    this.vatValue = vatValue;
-    this.vatRate = vatRate;
-  }
 
 }
