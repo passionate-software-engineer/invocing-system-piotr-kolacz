a4a24ce6cf5ac257f3e4c4a37beeff0d79bc53c2
Piotr Kolacz <pkolacz@gradle.com>
2021-02-09 14:56:42 +0100

    solution of task #4

--- src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
+++ src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
@@ -57,37 +57,43 @@ public class FileBasedDatabase implements Database {
   }
 
   @Override
-  public void update(int id, Invoice updatedInvoice) {
+  public Optional<Invoice> update(int id, Invoice updatedInvoice) {
     try {
       List<String> allInvoices = filesService.readAllLines(databasePath);
-      var listWithoutInvoiceWithGivenId = allInvoices
+      var invoicesWithoutInvoiceWithGivenId = allInvoices
           .stream()
           .filter(line -> !containsId(line, id))
           .collect(Collectors.toList());
 
-      if (allInvoices.size() == listWithoutInvoiceWithGivenId.size()) {
-        throw new IllegalArgumentException("Id " + id + " does not exist");
-      }
-
       updatedInvoice.setId(id);
-      listWithoutInvoiceWithGivenId.add(jsonService.toJson(updatedInvoice));
+      invoicesWithoutInvoiceWithGivenId.add(jsonService.toJson(updatedInvoice));
 
-      filesService.writeLinesToFile(databasePath, listWithoutInvoiceWithGivenId);
+      filesService.writeLinesToFile(databasePath, invoicesWithoutInvoiceWithGivenId);
+
+      allInvoices.removeAll(invoicesWithoutInvoiceWithGivenId);
+      return allInvoices.isEmpty() ? Optional.empty() : Optional.of(jsonService.toObject(allInvoices.get(0), Invoice.class));
 
     } catch (IOException ex) {
       throw new RuntimeException("Failed to update invoice with id: " + id, ex);
     }
+
   }
 
   @Override
-  public void delete(int id) {
+  public Optional<Invoice> delete(int id) {
     try {
-      var updatedList = filesService.readAllLines(databasePath)
+      var allInvoices = filesService.readAllLines(databasePath);
+
+      var invoicesExceptDeleted = allInvoices
           .stream()
           .filter(line -> !containsId(line, id))
           .collect(Collectors.toList());
 
-      filesService.writeLinesToFile(databasePath, updatedList);
+      filesService.writeLinesToFile(databasePath, invoicesExceptDeleted);
+
+      allInvoices.removeAll(invoicesExceptDeleted);
+
+      return allInvoices.isEmpty() ? Optional.empty() : Optional.of(jsonService.toObject(allInvoices.get(0), Invoice.class));
 
     } catch (IOException ex) {
       throw new RuntimeException("Failed to delete invoice with id: " + id, ex);
--- src/test/groovy/pl/futurecollars/invoicing/db/AbstractDatabaseTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/db/AbstractDatabaseTest.groovy
@@ -62,29 +62,27 @@ abstract class AbstractDatabaseTest extends Specification {
         database.getAll().isEmpty()
     }
 
-    def "deleting not existing invoice is not causing any error"() {
+    def "deleting not existing invoice returns optional empty"() {
         expect:
-        database.delete(123);
+        database.delete(123) == Optional.empty()
     }
 
-    def "it's possible to update the invoice"() {
+    def "it's possible to update the invoice, original invoice is returned"() {
         given:
-        int id = database.save(invoices.get(0))
+        def originalInvoice = invoices.get(0)
+        int id = database.save(originalInvoice)
 
         when:
-        database.update(id, invoices.get(1))
+        def result = database.update(id, invoices.get(1))
 
         then:
         database.getById(id).get() == invoices.get(1)
+        result == Optional.of(originalInvoice)
     }
 
-    def "updating not existing invoice throws exception"() {
-        when:
-        database.update(213, invoices.get(1))
-
-        then:
-        def ex = thrown(IllegalArgumentException)
-        ex.message == "Id 213 does not exist"
+    def "updating not existing invoice returns Optional.empty()"() {
+        expect:
+        database.update(213, invoices.get(1)) == Optional.empty()
     }
 
 }
