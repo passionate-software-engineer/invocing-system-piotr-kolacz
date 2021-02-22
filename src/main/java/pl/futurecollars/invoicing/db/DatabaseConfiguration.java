96f862e67391e509930328b34465150d8c06b667
Piotr Kolacz <pkolacz@gradle.com>
2021-02-22 09:50:48 +0100

    Task #7

--- src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
+++ src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
