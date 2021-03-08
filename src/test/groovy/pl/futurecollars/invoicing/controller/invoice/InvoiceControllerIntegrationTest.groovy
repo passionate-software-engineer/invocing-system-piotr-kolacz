4deeaa413abd9156595e67ff5f171cfc7bf34b59
Piotr Kolacz <pkolacz@gradle.com>
2021-03-08 17:38:23 +0100

    Task #11

--- src/test/groovy/pl/futurecollars/invoicing/controller/invoice/InvoiceControllerIntegrationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/controller/invoice/InvoiceControllerIntegrationTest.groovy
@@ -3,11 +3,13 @@ package pl.futurecollars.invoicing.controller.invoice
 
 import org.springframework.http.MediaType
 import pl.futurecollars.invoicing.controller.AbstractControllerTest
+import spock.lang.Unroll
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import static pl.futurecollars.invoicing.helpers.TestHelpers.invoice
 
+@Unroll
 class InvoiceControllerIntegrationTest extends AbstractControllerTest {
 
     def "empty array is returned when no invoices were added"() {
