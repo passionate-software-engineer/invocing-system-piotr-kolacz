8e1908f6db05b1821e24d81d2580d932fbd5415d
Piotr Kolacz <pkolacz@gradle.com>
2021-03-18 14:07:22 +0100

    Task #14

--- src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
+++ src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
@@ -1,9 +1,20 @@
 package pl.futurecollars.invoicing.db;
 
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +25,10 @@ import pl.futurecollars.invoicing.db.file.IdProvider;
 import pl.futurecollars.invoicing.db.jpa.InvoiceRepository;
 import pl.futurecollars.invoicing.db.jpa.JpaDatabase;
 import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
+import pl.futurecollars.invoicing.db.mongo.MongoBasedDatabase;
+import pl.futurecollars.invoicing.db.mongo.MongoIdProvider;
 import pl.futurecollars.invoicing.db.sql.SqlDatabase;
+import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.utils.FilesService;
 import pl.futurecollars.invoicing.utils.JsonService;
 
@@ -62,6 +76,43 @@ public class DatabaseConfiguration {
   @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "memory")
   public Database inMemoryDatabase() {
     return new InMemoryDatabase();
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "mongo")
+  public MongoDatabase mongoDb(
+      @Value("${invoicing-system.database.name}") String databaseName
+  ) {
+    CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+        fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+    MongoClientSettings settings = MongoClientSettings.builder()
+        .codecRegistry(pojoCodecRegistry)
+        .build();
+
+    MongoClient client = MongoClients.create(settings);
+    return client.getDatabase(databaseName);
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "mongo")
+  public MongoIdProvider mongoIdProvider(
+      @Value("${invoicing-system.database.counter.collection}") String collectionName,
+      MongoDatabase mongoDb
+  ) {
+    MongoCollection<Document> collection = mongoDb.getCollection(collectionName);
+    return new MongoIdProvider(collection);
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "mongo")
+  public Database mongoDatabase(
+      @Value("${invoicing-system.database.collection}") String collectionName,
+      MongoDatabase mongoDb,
+      MongoIdProvider mongoIdProvider
+  ) {
+    MongoCollection<Invoice> collection = mongoDb.getCollection(collectionName, Invoice.class);
+    return new MongoBasedDatabase(collection, mongoIdProvider);
   }
 
 }
