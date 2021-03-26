49a629f042541d0f7845d8c689c1a22409a690bd
Piotr Kolacz <pkolacz@gradle.com>
2021-03-26 11:00:44 +0100

    Task #17

--- angular-frontend/invoicing-app/src/app/app.component.ts
+++ angular-frontend/invoicing-app/src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import { Company } from "./company";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'invoicing-app';
+
+    companies: Company[] = [
+        new Company(
+            "111-111-11-11",
+            "ul. First 1",
+            "First Ltd.",
+            1111.11,
+            111.11
+        ),
+        new Company(
+            "222-222-22-22",
+            "ul. Second 2",
+            "Second Ltd.",
+            2222.22,
+            222.22
+        )
+    ];
+
+    newCompany: Company = new Company("", "", "", 0, 0);
+
+    addCompany() {
+        this.companies.push(this.newCompany);
+        this.newCompany = new Company("", "", "", 0, 0);
+    }
+
+    deleteCompany(companyToDelete: Company) {
+        this.companies = this.companies.filter(company => company !== companyToDelete);
+    }
+
+    triggerUpdate(company: Company) {
+        company.editedCompany = new Company(
+            company.taxIdentificationNumber,
+            company.address,
+            company.name,
+            company.healthInsurance,
+            company.pensionInsurance
+        )
+
+        company.editMode = true
+    }
+
+    cancelCompanyUpdate(company: Company) {
+        company.editMode = false;
+    }
+
+    updateCompany(updatedCompany: Company) {
+        updatedCompany.taxIdentificationNumber = updatedCompany.editedCompany.taxIdentificationNumber
+        updatedCompany.address = updatedCompany.editedCompany.address
+        updatedCompany.name = updatedCompany.editedCompany.name
+        updatedCompany.healthInsurance = updatedCompany.editedCompany.healthInsurance
+        updatedCompany.pensionInsurance = updatedCompany.editedCompany.pensionInsurance
+        
+        updatedCompany.editMode = false;
+    }
 }
---
+++ angular-frontend/invoicing-app/src/app/company.ts
@@ -0,0 +1,15 @@
+export class Company {
+
+    public editMode: boolean = false;
+    public editedCompany: Company = null;
+
+    constructor(
+        public taxIdentificationNumber: string,
+        public address: string,
+        public name: string,
+        public healthInsurance: number,
+        public pensionInsurance: number
+    ) {
+    }
+    
+}
