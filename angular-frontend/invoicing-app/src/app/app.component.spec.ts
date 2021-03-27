a35957137bb850523b91b02ae26391da38643c3b
Piotr Kolacz <pkolacz@gradle.com>
2021-03-27 19:17:12 +0100

    Task #18

--- angular-frontend/invoicing-app/src/app/app.component.spec.ts
+++ angular-frontend/invoicing-app/src/app/app.component.spec.ts
@@ -1,31 +1,87 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {CompanyService} from './company.service';
+import {Company} from './company';
+import {of} from 'rxjs';
+import {FormsModule} from '@angular/forms';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            providers: [
+                {provide: CompanyService, useClass: MockCompanyService}
+            ],
+            declarations: [
+                AppComponent
+            ],
+            imports: [
+                FormsModule
+            ]
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it(`should display a list of companies`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const component = fixture.componentInstance;
+        component.ngOnInit()
+        fixture.detectChanges();
 
-  it(`should have as title 'invoicing-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('invoicing-app');
-  });
+        expect(fixture.nativeElement.innerText).toContain("111-111-11-11	ul. First 1	First Ltd.	111.11	1111.11")
+        expect(fixture.nativeElement.innerText).toContain("222-222-22-22	ul. Second 2	Second Ltd.	222.22	2222.22")
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('invoicing-app app is running!');
-  });
+        expect(component.companies.length).toBe(2)
+        expect(component.companies).toBe(MockCompanyService.companies)
+
+        const taxIdInput: HTMLInputElement = fixture.nativeElement.querySelector("input[name=taxIdentificationNumber]")
+        taxIdInput.value = "333-333-33-33"
+        taxIdInput.dispatchEvent(new Event('input'));
+
+        const nameInput: HTMLInputElement = fixture.nativeElement.querySelector("input[name=name]")
+        nameInput.value = "Third Ltd."
+        nameInput.dispatchEvent(new Event('input'));
+
+        const addressInput: HTMLInputElement = fixture.nativeElement.querySelector("input[name=address]")
+        addressInput.value = "ul. Third 3"
+        addressInput.dispatchEvent(new Event('input'));
+
+        const addInvoiceBtn: HTMLElement = fixture.nativeElement.querySelector("#addCompanyBtn")
+        addInvoiceBtn.click()
+
+        fixture.detectChanges();
+
+        expect(fixture.nativeElement.innerText).toContain("333-333-33-33	ul. Third 3	Third Ltd.	0	0")
+    });
+
+    class MockCompanyService {
+
+        static companies: Company[] = [
+            new Company(
+                1,
+                "111-111-11-11",
+                "ul. First 1",
+                "First Ltd.",
+                1111.11,
+                111.11
+            ),
+            new Company(
+                2,
+                "222-222-22-22",
+                "ul. Second 2",
+                "Second Ltd.",
+                2222.22,
+                222.22
+            )
+        ];
+
+        getCompanies() {
+            return of(MockCompanyService.companies)
+        }
+
+        addCompany(company: Company) {
+            MockCompanyService.companies.push(company)
+            return of()
+        }
+
+    }
+
 });
