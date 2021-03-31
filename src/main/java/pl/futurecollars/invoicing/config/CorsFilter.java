611dd946555fa08e911d26ec7adf030b9fb2beaf
Piotr Kolacz <pkolacz@gradle.com>
2021-03-31 10:02:31 +0200

    Task #19

--- src/main/java/pl/futurecollars/invoicing/config/CorsFilter.java
+++ src/main/java/pl/futurecollars/invoicing/config/CorsFilter.java
@@ -5,16 +5,20 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class CorsFilter extends OncePerRequestFilter {
 
+  @Value("${invoicing-system.allowedOrigins}")
+  private String allowedOrigins;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    addHeaderIfAbsent("Access-Control-Allow-Origin", "http://localhost:4200", response);
+    addHeaderIfAbsent("Access-Control-Allow-Origin", allowedOrigins, response);
     addHeaderIfAbsent("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS", response);
     addHeaderIfAbsent("Access-Control-Max-Age", "3600", response);
     addHeaderIfAbsent("Access-Control-Allow-Headers", "content-type, x-xsrf-token", response);
